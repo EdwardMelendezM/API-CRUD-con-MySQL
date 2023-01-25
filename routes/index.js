bfd549520d7323f28445c92cad60dc3221c99026
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-01-25 14:02:57 -0500

    Subiendo proyecto

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var movies = require('../models/movies'),
+    express = require('express'),
+    router = express.Router();
+
+function error404(req, res, next) {
+    let error = new Error(),
+        locals = {
+            title: 'Error 404',
+            description: 'Recurso no encontrado',
+            error: error
+        }
+    error.status = 404;
+    res.render('error', locals)
+
+}
+
+router
+    .use(movies)
+    .get('/', function (req, res, next) {
+        req.getConnection((err, movies) => {
+            movies.query('SELECT* FROM movie', function (err, rows) {
+                let locals = {
+                    title: 'Lista de Peliculas',
+                    data: rows
+                }
+                res.render('index', locals);
+            })
+        })
+
+    })
+    .use(error404)
+module.exports = router;
